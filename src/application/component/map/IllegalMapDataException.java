fd7e949fbb49c61883547232e7497d03523f6aad
Higman <s15t201@stu.kagawa-u.ac.jp>
2017-10-31 22:03:43 +0900

    implement createMap method and some method related to the method

--- src/application/component/map/IllegalMapDataException.java
+++ src/application/component/map/IllegalMapDataException.java
@@ -1,4 +1,13 @@
 package application.component.map;
 
-public class IllegalMapDataException {
+public class IllegalMapDataException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public IllegalMapDataException() {
+        System.out.println("不適切なMapデータです");
+    }
+
+    public IllegalMapDataException(String message) {
+        super(message);
+    }
 }
---
+++ src/application/component/map/MapInformation.java
@@ -0,0 +1,31 @@
+package application.component.map;
+
+import java.net.URL;
+
+public enum MapInformation {
+    STAGE1(ClassLoader.getSystemResource("map/stage1.dat"));
+
+    private final URL url;  // ステージのリソース位置
+
+    MapInformation(URL fileURL) {
+        this.url = fileURL;
+    }
+
+    /**
+     * リソースのURLを取得
+     *
+     * @return
+     */
+    public URL getURL() {
+        return url;
+    }
+
+    /**
+     * 対応する番号を取得
+     *
+     * @return 連番
+     */
+    public int getNumber() {
+        return ordinal() + 1;
+    }
+}
