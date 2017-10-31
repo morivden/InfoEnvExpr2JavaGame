6219bb684c42dae9313303f508f301a3d1490059
makimakitaitai <s15t266@stu.kagawa-u.ac.jp>
2017-10-31 18:16:52 +0900

    Scene作成のプログラムに修正。(最初のコミットは勘違いしてました。)

--- src/application/controller/TitleController.java
+++ src/application/controller/TitleController.java
@@ -1,28 +1,45 @@
 package application.controller;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
+import application.Main;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.control.*;
+import javafx.scene.text.Text;
+import java.io.IOException;
 
-public class TitleController extends Application {
-	
-	public static final int WINDOW_HEIGHT = 400;
-    public static final int WINDOW_WIDTH = 600;
-    public static Stage primaryStage;
+public class TitleController {
 
-    @Override
-    public void start(Stage primaryStage) {
+    @FXML private Text gameTitle;
+    @FXML private Button startButton;
+    @FXML private Button configBottun;
+
+    private static final TitleController tc;  // GameControllerインスタンス
+    private static final Scene SCENE;
+
+    static  {
+        FXMLLoader fxmlLoader = new FXMLLoader(ClassLoader.getSystemResource("fxml/TitleController.fxml"));
         try {
-            this.primaryStage = primaryStage;
-            this.primaryStage.setWidth(WINDOW_WIDTH);
-            this.primaryStage.setHeight(WINDOW_HEIGHT);
-            this.primaryStage.setTitle("JavaGame");  // タイトル設定
-            this.primaryStage.show();
-        } catch (Exception e) {
+            fxmlLoader.load();
+        } catch (IOException e) {
             e.printStackTrace();
         }
+        Parent parent = fxmlLoader.getRoot();
+        Scene scene = new Scene(parent);
+
+        SCENE = scene;
+        tc = fxmlLoader.getController();
     }
 
-    public static void main(String[] args) {
-        launch(args);
+    public static TitleController getInstance() {
+        return tc;
+    }
+
+    public void show() {
+        TitleControllerMain.primaryStage.setScene(SCENE);
     }
 }
