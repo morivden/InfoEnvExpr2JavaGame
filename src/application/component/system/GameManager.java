387e96a24075deb71b7627c35fdad3d3a16a60d8
Higman <s15t201@stu.kagawa-u.ac.jp>
2017-11-01 16:16:54 +0900

    init commit

---
+++ src/application/component/system/GameManager.java
@@ -0,0 +1,12 @@
+package application.component.system;
+
+public class GameManager {
+    private static GameManager ourInstance = new GameManager();
+
+    public static GameManager getInstance() {
+        return ourInstance;
+    }
+
+    private GameManager() {
+    }
+}
