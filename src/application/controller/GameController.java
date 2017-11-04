7cf6b8bf0095fb1bea7e99e4736510531e651eea
Higman <s15t201@stu.kagawa-u.ac.jp>
2017-11-04 15:03:42 +0900

    add key evets,key Press and Release , to GameController class

--- src/application/controller/GameController.java
+++ src/application/controller/GameController.java
@@ -1,20 +1,22 @@
 package application.controller;
 
-import application.Main;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class GameController {
+public class GameController implements Initializable {
 
-    @FXML
-    private Pane drawPane;
+    @FXML private AnchorPane root;
+    @FXML private Pane drawPane;
 
     private static final GameController gc;  // GameControllerインスタンス
     private static final Scene SCENE;
@@ -33,11 +35,68 @@ public class GameController {
         gc = fxmlLoader.getController();
     }
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        drawPane.setFocusTraversable(true);
+        drawPane.requestFocus();
+
+        if ( drawPane.isFocused() ) {
+            System.out.println("aa");
+        }
+    }
+
     public static GameController getInstance() {
         return gc;
     }
 
     public void show() {
         GameControllerMain.primaryStage.setScene(SCENE);
+    }
+
+    /**
+     * キーボードのキーが押された時の処理
+     *
+     * @param event the event
+     */
+    @FXML
+    public void onKeyPressed(KeyEvent event) {
+        System.out.print("Pressed  ");
+        switch ( event.getCode() ) {
+            case UP :
+                System.out.println("うえ");      break;
+            case DOWN :
+                System.out.println("した");      break;
+            case LEFT :
+                System.out.println("ひだり");    break;
+            case RIGHT :
+                System.out.println("みぎ");      break;
+            case SPACE :
+                System.out.println("すぺーす");  break;
+            default :
+                System.out.println("なし");      break;
+        }
+    }
+
+    /**
+     * キーボードのキーが離された時の処理
+     *
+     * @param event the event
+     */
+    public void onKeyReleased(KeyEvent event) {
+        System.out.print("Released: ");
+        switch ( event.getCode() ) {
+            case UP :
+                System.out.println("うえ");      break;
+            case DOWN :
+                System.out.println("した");      break;
+            case LEFT :
+                System.out.println("ひだり");    break;
+            case RIGHT :
+                System.out.println("みぎ");      break;
+            case SPACE :
+                System.out.println("すぺーす");  break;
+            default :
+                System.out.println("なし");      break;
+        }
     }
 }
