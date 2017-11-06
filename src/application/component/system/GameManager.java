b51190c4ac34538bcaf19807a6065dec738212f9
Higman <s15t201@stu.kagawa-u.ac.jp>
2017-11-07 02:19:09 +0900

    ゲーム実行部で、Threadの代わりにTimerを用いるように変更

--- src/application/component/system/GameManager.java
+++ src/application/component/system/GameManager.java
@@ -5,6 +5,8 @@ import application.component.map.IllegalMapDataException;
 import application.component.map.MapFactory;
 import javafx.scene.layout.Pane;
 
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
@@ -17,15 +19,12 @@ public class GameManager {
     private int stageNum;     // 現在選択肢ているステージ番号
     private Pane drawPane;    // 使用するパネル
 
-    private AtomicBoolean running;  // 実行フラグ
-    private GameProcess gp;         // ゲームプロセス
+    private AtomicBoolean is_continuing = new AtomicBoolean(false);  // ゲームの持続判定
+    private GameProcessTask gpt;    // ゲームプロセス
+    private Timer gameTimer;        // ゲームプロセス用タイマー
 
     private GameMap gameMap;        // マップ
     // TODO Playerクラスのフィールドを作成
-
-    private GameManager() {
-        running = new AtomicBoolean(false);
-    }
 
     public static GameManager getInstance(Pane drawPane, int stageNum) {
         ourInstance.stageNum = stageNum;
@@ -37,13 +36,13 @@ public class GameManager {
      * 開始メソッド
      */
     public void start() {
-        if ( !running.get() ) { return; }  // 実行中かどうか判定
-        running.set(true);  // フラグ更新
-
-        try {
-            exec();             // ゲーム実行
-        } catch (NotGameProcessException e) {
-            e.printStackTrace();
+        // 実行中でなければ、開始
+        if ( gameTimer == null ) {
+            try {
+                exec();             // ゲーム実行
+            } catch ( NotGameProcessException e ) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -51,24 +50,36 @@ public class GameManager {
      * 停止メソッド
      */
     public void stop() {
-        running.set(false);  // フラグ更新
+        // ゲームが実行中であれば、停止
+        if ( gameTimer != null ) {
+            gameTimer.cancel();  // 停止
+            gameTimer.purge();   // タスクの消去
+            gameTimer = null;    // 破棄
+        }
     }
 
     /**
-     * 現在保持しているゲームの状態を削除
+     * 現在のゲーム状態を削除
      */
     public void delete() {
-        stop();      // 停止
-        gp = null;   // 現在のデータを削除
+        stop();  // 停止
+        is_continuing.set(false);  // ゲームの状態の継続フラグを下ろす
     }
 
     /**
      * 実行メソッド
-     *
      */
     private void exec() throws NotGameProcessException {
-        if ( gp == null ) { initializeGameComponent(stageNum); }  // ゲーム実行
-        gp.start();
+        // ゲーム状態の新規作成判定
+        if ( is_continuing.get() ) {
+            initializeGameComponent(stageNum);  // 初期化
+            is_continuing.set(true);            // ゲーム状態の継続フラグを立てる
+        }
+
+        // 実行
+        gameTimer = new Timer("game_timer", true);
+        gpt = new GameProcessTask();
+        gameTimer.scheduleAtFixedRate(gpt, 0, PROCESS_INTERVAL_MILLISECOND);  // 実行
     }
 
     /**
@@ -77,18 +88,16 @@ public class GameManager {
     private void initializeGameComponent(int stageNum) {
         try {
             gameMap = MapFactory.createMap(stageNum);
-        } catch (IllegalMapDataException e) {
+        } catch ( IllegalMapDataException e ) {
             e.printStackTrace();
             // TODO 例外が発生した場合の仮マップを用意する。仮マップは、GameFactoryクラスかGameMapクラスのクラスメソッドから取得する
         }
         // TODO 生成したMapクラスのインスタンスからPlayerクラスのインスタンスを取得し、Playerフィールドに格納
         inputObjectsToPane();    // drawPaneの初期化
-        gp = new GameProcess();  // 新しい実行クラスのインスタンスの生成
     }
 
     /**
      * GameMapクラスのインスタンスが持つImageViewをdrawPaneに登録する
-     *
      */
     private void inputObjectsToPane() {
         // TODO 実装する
@@ -97,26 +106,18 @@ public class GameManager {
     /**
      * ゲーム部クラス
      */
-    private class GameProcess extends Thread {
+    private class GameProcessTask extends TimerTask {
         @Override
         public void run() {
-            while ( running.get() ) {
-                // TODO 各ゲームプロセスの実装
-                //== ファクトリーの更新
-                //== キャラクターの更新
-                //== 移動オブジェクトの更新
-                //== 攻撃オブジェクトの更新
-                //== 衝突オブジェクトの反映
-                //== 無効キャラクターの削除
-                //== ゲーム終了判定
-
-                //== 待ち
-                try {
-                    sleep(PROCESS_INTERVAL_MILLISECOND);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
+            // TODO 各ゲームプロセスの実装
+            //== ファクトリーの更新
+            //== キャラクターの更新
+            //== 移動オブジェクトの更新
+            //== 攻撃オブジェクトの更新
+            //== 衝突オブジェクトの反映
+            //== 描画パネル(drawPane)の移動
+            //== 無効キャラクターの削除
+            //== ゲーム終了判定
         }
     }
 
