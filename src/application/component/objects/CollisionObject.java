2de573f1ca65af92545bf7f591d72e0f788b6450
morivden <fuziyamavolcano@yahoo.co.jp>
2017-11-07 16:05:13 +0900

    implement CollisionObjectAbstractClass

--- src/application/component/objects/CollisionObject.java
+++ src/application/component/objects/CollisionObject.java
@@ -3,22 +3,51 @@ package application.component.objects;
 import java.util.List;
 
 public abstract class CollisionObject {
-    public List<CollisionObject> collisionObjects;
+    public static List<CollisionObject> collisionObjects;
     
     
     public CollisionObject() {
         
     }
     
+    /**
+     * オブジェクトごとの衝突チェック
+     * @param gameObject
+     */
     public static void checkCollisions(GameObject gameObject) {
+        // 衝突してるかをチェック
+        // チェックしたいCollisionObjectをcollisionObjectsからピックアップ
+        for (CollisionObject pickupCollisionObject : collisionObjects) {
+            // 全てのcollisionObjectsを見ていく
+            for (CollisionObject collisionObject : collisionObjects) {
+                // GameObject側からCollisionObjectの呼び出し
+                CollisionObject collisionGameObject = gameObject.getCollisionObject();
+                // 同じオブジェクトの場合はcontinue;
+                if (pickupCollisionObject.equals(collisionGameObject)) {
+                    continue;
+                }
+                // 衝突していたらigniteEventsを呼び出す
+                if (pickupCollisionObject.isCollide(collisionGameObject)) {
+                    pickupCollisionObject.igniteEvents(gameObject);
+                }
+            }
+        }
         
+        // 衝突してたらiginteEventsで発火
     }
     
-    public void igniteEvents(GameObject gameObject) {
-        
-    }
+    public abstract void igniteEvents(GameObject gameObject);
     
-    public void transfer(double x, double y) {
-        
-    }
+    public abstract boolean isCollide(CollisionObject collisionObject);/** {
+        if (collisionObject.instanceOf(RectangleCollisionObject)) {
+            // TODO 後でgetRectangleメソッドを実装
+            // 型キャストしてgetRectangleメソッドを呼び出す
+            // ((RectangleCollisionObject)collisionObject).getRectangle();
+        }
+        return true;
+    }**/
+    
+    
+    // 座標移動
+    public abstract void transfer(double x, double y);
 }
