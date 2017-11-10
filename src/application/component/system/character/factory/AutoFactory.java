d992f72ddc7ced73ffc2443ced4dcb4ca0aaf507
Higman <s15t201@stu.kagawa-u.ac.jp>
2017-11-10 16:25:58 +0900

    tmp commit

--- src/application/component/system/character/factory/AutoFactory.java
+++ src/application/component/system/character/factory/AutoFactory.java
@@ -3,6 +3,8 @@ package application.component.system.character.factory;
 import application.component.system.character.controller.Enemy;
 
 public class AutoFactory implements CharacterFactory<Enemy> {
+//    private List<PlayableCharacter> createdCharacterList = new ArrayList<>();  // 自身が作成したキャラクターのインスタンス
+
     @Override
     public void updateAll() {
 
