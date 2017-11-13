911bdd7d5e3b28778fde72b82de1c0bf3e9a347d
Higman <s15t201@stu.kagawa-u.ac.jp>
2017-11-13 13:41:19 +0900

    キャラクター生成部のメソッドを一部実装した

--- src/application/component/system/character/controller/Player.java
+++ src/application/component/system/character/controller/Player.java
@@ -1,6 +1,14 @@
 package application.component.system.character.controller;
 
+import application.component.objects.character.PlayableCharacter;
+
 public class Player implements CharacterController {
+    private PlayableCharacter character;
+
+    public Player(PlayableCharacter character) {
+        this.character = character;
+    }
+
     @Override
     public void update() {
 
