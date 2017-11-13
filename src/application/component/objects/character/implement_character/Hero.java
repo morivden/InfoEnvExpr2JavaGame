a0e1532290a0818ed2653a56a0ec46072969caf4
makimaki <s15t266@stu.kagawa-u.ac.jp>
2017-11-13 13:44:38 +0900

    Revert "キャラクター生成部の作成"

--- src/application/component/objects/character/implement_character/Hero.java
+++
@@ -1,6 +0,0 @@
-package application.component.objects.character.implement_character;
-
-import application.component.objects.character.PlayableCharacter;
-
-public class Hero extends PlayableCharacter {
-}
