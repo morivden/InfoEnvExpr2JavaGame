a34de59687f36439daec90585c58e912d3036193
morivden <fuziyamavolcano@yahoo.co.jp>
2017-11-17 16:32:23 +0900

    change Point2D import

--- src/application/component/objects/character/implement_character/TMPCharacter.java
+++ src/application/component/objects/character/implement_character/TMPCharacter.java
@@ -1,7 +1,7 @@
 package application.component.objects.character.implement_character;
 
 import application.component.objects.character.PlayableCharacter;
-import javafx.geometry.Point2D;
+import com.sun.javafx.geom.Point2D;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
@@ -11,7 +11,7 @@ public class TMPCharacter extends PlayableCharacter {
     public static int DEFAULT_SPEED = 10;
     public static int RANGE = 200;
 
-    private Point2D speed = new Point2D(0.0, 0.0);
+    private Point2D speed = new Point2D(0, 0);
 
     public TMPCharacter(Point2D pos) {
         position = pos;
@@ -22,10 +22,10 @@ public class TMPCharacter extends PlayableCharacter {
     }
 
     private void updateImg() {
-        img.setTranslateX(position.getX());
-        img.setTranslateY(position.getY());
-        range.setTranslateX(position.getX());
-        range.setTranslateY(position.getY());
+        img.setTranslateX(position.x);
+        img.setTranslateY(position.y);
+        range.setTranslateX(position.x);
+        range.setTranslateY(position.y);
     }
 
     public Circle getRange() {
@@ -33,16 +33,17 @@ public class TMPCharacter extends PlayableCharacter {
     }
 
     public void move() {
-        position = position.add(speed.getX(), speed.getY());
+        // position = position.setLocation(speed.x, speed.y);
+        position.setLocation(position.x + speed.x, position.y + speed.y);
         updateImg();
     }
 
     public void setSpeed(int x_speed, int y_speed) {
-        speed = new Point2D(speedCheck(x_speed), speedCheck(y_speed));
+        speed.setLocation(speedCheck(x_speed), speedCheck(y_speed));
     }
 
     public int getX_speed() {
-        return (int)speed.getX();
+        return (int)speed.x;
     }
 
     public Circle getImg() {
@@ -50,7 +51,7 @@ public class TMPCharacter extends PlayableCharacter {
     }
 
     public int getY_speed() {
-        return (int)speed.getY();
+        return (int)speed.y;
 
     }
 
